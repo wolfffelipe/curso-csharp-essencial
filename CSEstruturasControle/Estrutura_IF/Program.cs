a2cc1be1c6e594412ee6d638a8261f6b1503584e
felipe.wolff <wolff.felipe@hotmail.com>
2024-02-06 10:40:49 -0300

    Novas aulas

---
+++ CSEstruturasControle/Estrutura_IF/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Estrutura IF\n\n");
+
+//var resposta2 = Convert.ToBoolean(Console.ReadLine());
+//resposta = resposta.ToLower();
+/*
+if (resposta2){
+    Console.WriteLine("Cliente é especial");
+}else{
+    Console.WriteLine("Cliente não é especial");
+}
+*/
+
+
+/* Variáveis */
+
+double saldo = 1000;
+
+
+/* Saque e saldo */
+
+Console.WriteLine($"Saldo da conta: R$ {saldo}");
+Console.WriteLine("Digite um valor para sacar: ");
+double saque = Convert.ToDouble(Console.ReadLine());
+
+if (saque > saldo){
+    Console.WriteLine("Saldo insuficiente");
+}else{
+    saldo = saldo - saque;
+}
+
+Console.WriteLine($"Seu saldo atual é de R$ {saldo}");
+
+
+/* Comparando números */
+
+Console.WriteLine("\n\nDigite um número inteiro:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número inteiro:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} é maior que {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"{num1} é menor que {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} é igual a {num2}");
+}
---
+++ CSEstruturasControle/Estrutura_Switch/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("SWITCH\n");
+
+var menu = 0;
+double num1, num2;
+
+Console.WriteLine("Digite dois números:");
+num1 = Convert.ToDouble(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\nEscolha uma opção");
+Console.Write("1. Somar\n2. Subtrair\n3. Multiplicar\n4. Dividir\n0. Sair\n");
+menu = Convert.ToInt32(Console.ReadLine());
+
+//switch (numero % 2) -- se 0 é par e se 1 é ímpar
+switch(menu)
+{
+    case 0:
+        break;
+    case 1:
+        Console.WriteLine($"\nSoma: {num1 + num2}");
+        break;
+    case 2:
+        Console.WriteLine($"\nSubtração: {num1 - num2}");
+        break;
+    case 3:
+        Console.WriteLine($"\nMultiplicação: {num1 * num2}");
+        break;
+    case 4:
+        Console.WriteLine($"\nDivisão: {num1 / num2}");
+        break;
+    default: Console.WriteLine("\nMenu não existe");
+        break;
+}
+
+Console.WriteLine("\n\nFim do processamento!");
+Console.ReadKey();
